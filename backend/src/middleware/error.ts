5cf251f48a3f83194d13b27483d0420cd1810ec1
Tobias Wälde <tobias.waelde@gmail.com>
2023-01-12 13:29:46 +0100

    improve error handling

--- backend/src/middleware/error.ts
+++ backend/src/middleware/error.ts
@@ -1,12 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 import logger from 'tw-logger';
 import { ZodError } from 'zod';
+import { ApiError } from '../types/error';
 
 /**
  * Middleware to catch all unhandled exceptions
  * @returns Express middleware function
  */
 export const errorHandler = () => (err: any, req: Request, res: Response, next: NextFunction) => {
+	// API error
+	if (err instanceof ApiError) {
+		logger.error(err.message);
+		return res.status(err.code).send(err.message);
+	}
+
 	// validation error
 	if (err instanceof ZodError) {
 		return res.status(400).send(err.format());
