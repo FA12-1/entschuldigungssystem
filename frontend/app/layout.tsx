a9912689dc2b231e21abe9601e1b388cfaadebf8
Jan <jf@ambimax.de>
2023-01-13 08:37:32 +0100

    wip: add side menu

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,12 +1,62 @@
+"use client";
+
+import {
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+} from "@mui/material";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import EmailIcon from "@mui/icons-material/Email";
+import useLocalStorage from "../hooks/useLocalStorage";
+import { styled } from "@mui/material/styles";
+
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html>
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+    const [open, setOpen] = useLocalStorage("drawer_open", true);
+
+    return (
+        <html>
+            <head />
+            <body>
+                <Drawer variant="persistent" anchor="left" open={open}>
+                    <DrawerHeader>
+                        <IconButton onClick={() => setOpen(!open)}>
+                            <ChevronLeftIcon />
+                        </IconButton>
+                    </DrawerHeader>
+                    <List>
+                        {["Inbox", "Starred", "Send email", "Drafts"].map(
+                            (text, index) => (
+                                <ListItem key={text} disablePadding>
+                                    <ListItemButton>
+                                        <ListItemIcon>
+                                            <EmailIcon />
+                                        </ListItemIcon>
+                                        <ListItemText primary={text} />
+                                    </ListItemButton>
+                                </ListItem>
+                            )
+                        )}
+                    </List>
+                </Drawer>
+                {children}
+            </body>
+        </html>
+    );
 }
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+}));
