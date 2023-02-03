8dd4f28b5cf434bee272147897298fad1523b410
Leandra Masi <lm@ambimax.de>
2023-02-03 09:39:50 +0100

    add routing pages for edit forms

---
+++ frontend/app/edit/class/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import type { NextPage } from "next";
+import EditClassForm from "../../../components/pages/edit/class/form";
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <EditClassForm />
+        </>
+    );
+};
+
+export default Home;
+
---
+++ frontend/app/edit/student/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import type { NextPage } from "next";
+import EditStudentForm from "../../../components/pages/edit/student/form";
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <EditStudentForm />
+        </>
+    );
+};
+
+export default Home;
