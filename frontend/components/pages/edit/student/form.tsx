61c65f755601cf07e00d7bfbcfc6bd005e9a66f0
Leandra Masi <lm@ambimax.de>
2023-02-03 09:41:15 +0100

    implement edit forms

---
+++ frontend/components/pages/edit/student/form.tsx
@@ -0,0 +1,66 @@
+import { Grid, TextField, Button, FormLabel } from "@mui/material";
+import { FC, useState } from "react";
+import { blue } from '@mui/material/colors';
+
+const EditStudentForm: FC = () => {
+    const color = blue[700];
+    const [selectedOption, setSelectedOption] = useState<String>();
+
+  // This function is triggered when the select changes
+  const selectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    setSelectedOption(value);
+  };
+
+    return (
+        <>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sx={{backgroundColor: color, color:"white"}}>
+                    <h1>Schüler bearbeiten</h1>
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField fullWidth label="Vorname"/>
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField fullWidth label="Nachname"/>
+                </Grid>
+                <Grid item xs={12}>
+                <TextField
+                        fullWidth
+                        type="email"
+                        placeholder="max@mustermann.de"
+                        label="E-Mail"
+                />
+                </Grid>
+                <Grid item xs={12}>
+                <TextField 
+                    fullWidth
+                    InputLabelProps={{ shrink: true }} 
+                    type="date"
+                    label="Geburtsdatum"
+                />
+                </Grid>
+                <Grid item xs={12}>
+                    <FormLabel 
+                        component="legend" 
+                        sx={{margin: "2% 0"}}>
+                            Klasse zuweisen
+                    </FormLabel>
+                    <select onChange={selectChange} id="Klasse">
+                    <option selected disabled>Klasse zuweisen</option>
+                    </select>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button 
+                        type="submit" 
+                        variant="contained" 
+                        sx={{backgroundColor: color}}>
+                            Änderungen speichern
+                    </Button>
+                </Grid>
+            </Grid>
+        </>
+    );
+};
+
+export default EditStudentForm;
---
+++ frontend/components/pages/edit/user/form.tsx
@@ -0,0 +1,82 @@
+import { Grid, TextField, Button, Checkbox, FormGroup, FormControl, FormControlLabel, FormHelperText, FormLabel } from "@mui/material";
+import { FC, useState } from "react";
+import * as React from "react";
+import { blue } from '@mui/material/colors';
+
+const EditUserForm: FC = () => {
+    const color = blue[700];
+    const [state, setState] = React.useState({
+        lehrer: true,
+        admin: false,
+      });
+    
+      const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setState({
+          ...state,
+          [event.target.name]: event.target.checked,
+        });
+      };
+    
+      const { lehrer, admin } = state;
+      const checkboxError = [lehrer, admin].filter((v) => v).length !== 1;
+
+    return (
+        <>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sx={{backgroundColor: color, color:"white"}}>
+                    <h1>Benutzer bearbeiten</h1>
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField fullWidth label="Vorname"/>
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField fullWidth label="Nachname"/>
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        fullWidth
+                        type="email"
+                        placeholder="max@mustermann.de"
+                        label="E-Mail"
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <FormControl
+                        required
+                        error={checkboxError}
+                        component="fieldset"
+                        sx={{ m: 3 }}
+                        variant="standard"
+                    >
+                        <FormLabel component="legend">Rolle</FormLabel>
+                        <FormGroup>
+                        <FormControlLabel
+                            control={
+                                <Checkbox checked={lehrer} onChange={handleChange} name="lehrer" />
+                            }
+                            label="Lehrer"
+                        />
+                        <FormControlLabel
+                            control={
+                                <Checkbox checked={admin} onChange={handleChange} name="admin" />
+                            }
+                            label="Admin"
+                        />
+                        </FormGroup>
+                        <FormHelperText>Bitte Rolle auswählen</FormHelperText>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button 
+                        type="submit" 
+                        variant="contained" 
+                        sx={{backgroundColor: color}}>
+                            Änderungen speichern
+                    </Button>
+                </Grid>
+            </Grid>
+        </>
+    );
+};
+
+export default EditUserForm;
