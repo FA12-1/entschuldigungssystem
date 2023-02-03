b6cb3b976f3563f7e9be6f493ad3e2316e82bfd1
Leandra Masi <lm@ambimax.de>
2023-02-03 09:40:12 +0100

    add routing pages for register forms

---
+++ frontend/app/register/class/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import type { NextPage } from "next";
+import RegisterClassForm from "../../../components/pages/register/class/form";
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <RegisterClassForm />
+        </>
+    );
+};
+
+export default Home;
