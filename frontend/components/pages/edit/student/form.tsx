158b7b81ff03b7ace72767902a7c7273073a7e20
Jan <jf@ambimax.de>
2023-04-28 10:09:39 +0200

    adjust form styling

--- frontend/components/pages/edit/student/form.tsx
+++ frontend/components/pages/edit/student/form.tsx
@@ -1,61 +1,64 @@
-import { Grid, TextField, Button, FormLabel } from "@mui/material";
+import { Grid, TextField, Button, FormLabel, Typography } from "@mui/material";
 import { FC, useState } from "react";
-import { blue } from '@mui/material/colors';
+import { blue } from "@mui/material/colors";
 
 const EditStudentForm: FC = () => {
     const color = blue[700];
     const [selectedOption, setSelectedOption] = useState<String>();
 
-  // This function is triggered when the select changes
-  const selectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const value = event.target.value;
-    setSelectedOption(value);
-  };
+    // This function is triggered when the select changes
+    const selectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const value = event.target.value;
+        setSelectedOption(value);
+    };
 
     return (
         <>
-            <Grid container maxWidth="sm" spacing={2} sx={{marginX: "auto"}}>
-                <Grid item xs={12} sx={{backgroundColor: color, color:"white"}}>
-                    <h1>Schüler bearbeiten</h1>
+            <Grid container spacing={1}>
+                <Grid
+                    item
+                    xs={12}
+                    sx={{ backgroundColor: color, color: "white" }}
+                >
+                    <Typography variant="h4">Schüler bearbeiten</Typography>
                 </Grid>
-                <Grid item xs={12} sx={{paddingRight: "16px"}}>
-                    <TextField fullWidth label="Vorname"/>
+                <Grid item xs={12}>
+                    <TextField fullWidth label="Vorname" />
                 </Grid>
-                <Grid item xs={12} sx={{paddingRight: "16px"}}>
-                    <TextField fullWidth label="Nachname"/>
+                <Grid item xs={12}>
+                    <TextField fullWidth label="Nachname" />
                 </Grid>
-                <Grid item xs={12} sx={{paddingRight: "16px"}}>
-                <TextField
+                <Grid item xs={12}>
+                    <TextField
                         fullWidth
                         type="email"
                         placeholder="max@mustermann.de"
                         label="E-Mail"
-                />
-                </Grid>
-                <Grid item xs={12} sx={{paddingRight: "16px"}}>
-                <TextField
-                    fullWidth
-                    InputLabelProps={{ shrink: true }}
-                    type="date"
-                    label="Geburtsdatum"
-                />
+                    />
                 </Grid>
                 <Grid item xs={12}>
-                    <FormLabel
-                        component="legend"
-                        sx={{margin: "2% 0"}}>
-                            Klasse zuweisen
-                    </FormLabel>
+                    <TextField
+                        fullWidth
+                        InputLabelProps={{ shrink: true }}
+                        type="date"
+                        label="Geburtsdatum"
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <FormLabel component="legend">Klasse zuweisen</FormLabel>
                     <select onChange={selectChange} id="Klasse">
-                    <option selected disabled>Klasse zuweisen</option>
+                        <option selected disabled>
+                            Klasse zuweisen
+                        </option>
                     </select>
                 </Grid>
                 <Grid item xs={12}>
                     <Button
                         type="submit"
                         variant="contained"
-                        sx={{backgroundColor: color}}>
-                            Änderungen speichern
+                        sx={{ backgroundColor: color }}
+                    >
+                        Änderungen speichern
                     </Button>
                 </Grid>
             </Grid>
